e733f9e4d171c245d1e75c621ca2030b79a8d703
Ashish David <58704058+AshishDavid@users.noreply.github.com>
2021-10-14 17:13:36 +0300

    second

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
-#!usr/bin/python3
 from flask import Flask,render_template,url_for,request, redirect
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#add database
+
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///tasks.db'
 
-#initialize db
 
 db = SQLAlchemy(app)
 
-#create model
+
 class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	task = db.Column(db.String(100),nullable = False)
 	date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
-	#create string
+	
 
 	def __repr__(self):
 		return '<Task %r>' %self.id
 
-#add task
+
 @app.route('/',methods=["POST","GET"])
 def index():
 	if request.method == "POST":
@@ -36,7 +34,7 @@ def index():
 			return "There was a problem adding your task"
 
 	else:
-		tasks = Todo.query.order_by(Todo.date_added).all() # fetchisng all data
+		tasks = Todo.query.order_by(Todo.date_added).all()
 		return render_template('index.html',title="Todo App", tasks = tasks)
 
 
@@ -51,10 +49,9 @@ def delete(id):
 		return "There was a problem in deleting that task"
 
 
-#add task
+
 @app.route('/update/<int:id>',methods=["POST","GET"])
 def update(id):
-	#get the task first for updating
 	task = Todo.query.get_or_404(id)
 	if request.method == "POST":
 		task.task = request.form['content']
