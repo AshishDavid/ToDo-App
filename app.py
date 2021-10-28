14207c468e0482da2769fc91f1c9ddf919c585c7
Ashish David <58704058+AshishDavid@users.noreply.github.com>
2021-10-29 00:01:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def update(id):
 		return render_template('update.html',title="Update", task = task)
 
 if __name__=="__main__":
-	app.run(debug=True)
+	app.run(debug=True, port=5000, host="0.0.0.0")
